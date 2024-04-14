852e56274a46382d155d66ac3848b02e09e26917
amity808 <bolarinwamuhdsodiq0@gmail.com>
2024-04-14 19:55:05 +0100

    add readme file

--- frontend-bank/src/helper/wagmiconfig.ts
+++ frontend-bank/src/helper/wagmiconfig.ts
@@ -1,6 +1,6 @@
 import { http, createConfig } from 'wagmi'
 import { mainnet, sepolia, celoAlfajores } from 'wagmi/chains'
-import { injected, metaMask, safe, walletConnect, coinbaseWallet, mock } from 'wagmi/connectors'
+import { injected, safe, walletConnect, coinbaseWallet } from 'wagmi/connectors'
 
 const projectId = "a3ee5bcc5e40e2e76756a65955636634"
 
